6000e38f9fabe88b5fca477418035aeba3d52e67
Mejia1994 <jrafa1994@gmail.com>
2022-05-08 17:30:43 -0600

    test

--- pages/__middleware.js
+++ pages/__middleware.js
@@ -1,6 +1,7 @@
+/*
 import {NextResponse} from 'next/server'
 
-/*
+/!*
 const redirectToLogin = function (req) {
     return NextResponse.redirect(new URL('/login', req.url))
 }
@@ -8,38 +9,38 @@ const redirectToLogin = function (req) {
 const redirectToHome = function (req) {
     return NextResponse.redirect(new URL('/home', req.url))
 }
-*/
+*!/
 
 const verifyToken = function (token) {
 
-    /* return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
+    /!* return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
          if (err) return {hasError: true, payload: err};
          else return {hasError: false, payload: decoded};
-     });*/
+     });*!/
 }
 
 export const middlewareWithAuthentication = function (req) {
 
-    /*let token = verifyToken(req.cookies?.token);
+    /!*let token = verifyToken(req.cookies?.token);
 
     if (token.hasError) {
         return redirectToLogin(req);
     }
-*/
+*!/
     return NextResponse.next();
 }
 
 export const middlewareWithOutAuthentication = function (req) {
 
-    /*let token = verifyToken(req.cookies?.token);
+    /!*let token = verifyToken(req.cookies?.token);
 
     if (!token.hasError) {
         return redirectToHome(req);
-    }*/
+    }*!/
 
     return NextResponse.next();
 }
 
 export default function _middleware() {
     return NextResponse.next();
-}
+}*/
