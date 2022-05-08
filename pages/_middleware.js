c1034f177a5eb730ff21ab5760e22711e7374b64
Mejia1994 <jrafa1994@gmail.com>
2022-05-08 17:52:49 -0600

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,28 +1,29 @@
 import {NextResponse} from 'next/server'
 import jwt from "jsonwebtoken";
 
-/*const redirectToLogin = function (req) {
+const redirectToLogin = function (req) {
     return NextResponse.redirect(new URL('/login', req.url))
 }
 
+/*
 const redirectToHome = function (req) {
     return NextResponse.redirect(new URL('/home', req.url))
 }*/
 
 const verifyToken = function (token) {
 
-     return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
-         if (err) return {hasError: true, payload: err};
-         else return {hasError: false, payload: decoded};
-     });
+    return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
+        if (err) return {hasError: true, payload: err};
+        else return {hasError: false, payload: decoded};
+    });
 }
 
 export const middlewareWithAuthentication = function (req) {
 
     let token = verifyToken(req.cookies?.token);
-
+    console.log(token);
     if (token.hasError) {
-       // return redirectToLogin(req);
+        return redirectToLogin(req);
     }
 
     return NextResponse.next();
@@ -36,7 +37,7 @@ export const middlewareWithOutAuthentication = function (req) {
         return redirectToHome(req);
     }*/
 
-   // return NextResponse.next();
+    // return NextResponse.next();
 }
 
 export default function _middleware() {
