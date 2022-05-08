77ba13e05e546bb88985e68e7dc7f71d6557fc41
Mejia1994 <jrafa1994@gmail.com>
2022-05-08 17:15:04 -0600

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -10,30 +10,31 @@ const redirectToHome = function (req) {
 }
 
 const verifyToken = function (token) {
-    return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
-        if (err) return {hasError: true, payload: err};
-        else return {hasError: false, payload: decoded};
-    });
+
+    /* return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
+         if (err) return {hasError: true, payload: err};
+         else return {hasError: false, payload: decoded};
+     });*/
 }
 
 export const middlewareWithAuthentication = function (req) {
 
-    let token = verifyToken(req.cookies?.token);
+    /*let token = verifyToken(req.cookies?.token);
 
     if (token.hasError) {
         return redirectToLogin(req);
     }
-
+*/
     return NextResponse.next();
 }
 
 export const middlewareWithOutAuthentication = function (req) {
 
-    let token = verifyToken(req.cookies?.token);
+    /*let token = verifyToken(req.cookies?.token);
 
     if (!token.hasError) {
         return redirectToHome(req);
-    }
+    }*/
 
     return NextResponse.next();
 }
