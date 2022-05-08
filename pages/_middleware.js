f56f918f92c38f7db9992f7bcbe131f2930de0d6
Mejia1994 <jrafa1994@gmail.com>
2022-05-08 17:49:10 -0600

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,32 +1,31 @@
 import {NextResponse} from 'next/server'
+import jwt from "jsonwebtoken";
 
-/*
-const redirectToLogin = function (req) {
+/*const redirectToLogin = function (req) {
     return NextResponse.redirect(new URL('/login', req.url))
 }
 
 const redirectToHome = function (req) {
     return NextResponse.redirect(new URL('/home', req.url))
-}
-*/
+}*/
 
 const verifyToken = function (token) {
 
-    /* return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
+     return jwt.verify(token, "process.env.JWT_SECRET", null, (err, decoded) => {
          if (err) return {hasError: true, payload: err};
          else return {hasError: false, payload: decoded};
-     });*/
+     });
 }
 
 export const middlewareWithAuthentication = function (req) {
 
-    /*let token = verifyToken(req.cookies?.token);
+    let token = verifyToken(req.cookies?.token);
 
     if (token.hasError) {
-        return redirectToLogin(req);
+       // return redirectToLogin(req);
     }
-*/
-   // return NextResponse.next();
+
+    return NextResponse.next();
 }
 
 export const middlewareWithOutAuthentication = function (req) {
